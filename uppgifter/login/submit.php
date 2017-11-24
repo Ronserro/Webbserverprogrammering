1e90b5ad6c41cb3c9ef39d3d26b2806f2309c417
Ronserro <christian.lind.te16c@gmail.com>
2017-11-24 10:11:10 +0100

    Början på kod som hämtar information

---
+++ uppgifter/login/submit.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbc = mysqli_connect("localhost","root","","form");
+
+$username = $_POST['username'];
+
+$password = $_POST['password'];
+
+$query = ;
+
+mysqli_query($dbc,$query);
+
+?>
