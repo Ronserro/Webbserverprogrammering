4ad464c2a697b80e848bb9da54f43e709331e1da
Ronserro <christian.lind.te16c@gmail.com>
2017-11-24 09:56:18 +0100

    Vanligt login formulär

---
+++ uppgifter/login/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+
+
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+        <form method="post" action="submit.php">
+            
+        Användarnamn: <input type="text" name="username">
+        Lösenord: <input type="password" name="password">
+        <input type="submit" name="submit">
+        
+        </form>
+
+
+    </body>
+
+
+
+
+</html>
