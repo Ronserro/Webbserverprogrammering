ba87d50f37deeaccea1597fd4bfbf402352c4aa6
Ronserro <christian.lind.te16c@gmail.com>
2017-11-24 11:04:42 +0100

    Skriver ut all information om användaren och kollar om personen finns i databasen

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -10,5 +10,43 @@ $query = "Select * from register where password='$password' and username='$usern
 
 $result = mysqli_query($dbc,$query);
 
+$print = mysqli_fetch_array($result);
 
+if(isset($print)){
+	echo "Successfully logged in!";
+	echo "<br><br><br>";
+	
+	echo "Användarnamn";
+	echo "<br>";
+	echo $print['username'];
+	echo "<br><br>";
+	
+	echo "Fullständigt namn";
+	echo "<br>";
+	echo $print['full_name'];
+	echo "<br><br>";
+	
+	echo "Mail";
+	echo "<br>";
+	echo $print['mail'];
+	echo "<br><br>";
+	
+	echo "Födelsedag";
+	echo "<br>";
+	echo $print['birthday'];
+	echo "<br><br>";
+	
+	echo "Lösenord";
+	echo "<br>";
+	echo $print['password'];
+	echo "<br><br>";
+}
+
+else{
+	echo "Your Username or password is wrong please try again";
+}
+
+if (isset($_SESSION['user'])) {
+  echo "logged in";
+ }
 ?>
