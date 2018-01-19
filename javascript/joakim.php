cbe81b59130a5ed47c916d3472f33d6e4bdb5dc6
Ronserro <christian.lind.te16c@gmail.com>
2018-01-19 13:14:47 +0100

    Uppg 1

---
+++ javascript/joakim.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+
+
+
+    <head>
+        <title>Jaok</title>
+        <meta charset="utf-8">
+		<script>
+		    function hejsan(){
+                alert("hejsan Joakim");
+            }
+		
+		
+		
+		</script>
+		
+		<style>
+		
+		
+		</style>
+		
+    </head>
+
+    <body>
+	
+	<button onclick="hejsan()">Alert</button><br><br>
+
+
+    </body>
+
+
+</html>
